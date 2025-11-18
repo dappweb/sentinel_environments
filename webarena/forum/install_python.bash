2cdb1285622b2cddfd6c48b2e9d0d64900b1a36a
Adam Fourney <adamfo@microsoft.com>
2025-11-18 21:35:04 +0000

    Updated README

--- webarena/forum/install_python.bash
+++
@@ -1,9 +0,0 @@
-export PYTHONUNBUFFERED=1
-apk upgrade --no-cache python3 
-apk add --no-cache build-base
-apk add --no-cache python3-dev
-apk add --no-cache postgresql-dev
-apk add --no-cache vim
-python3 -m ensurepip
-pip3 install --no-cache --upgrade pip setuptools
-pip install psycopg2
