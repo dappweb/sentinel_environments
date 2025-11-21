a53add5dcdcbb732e9b6fe1d3c386a3ca442bf50
Amanda Swearngin <mandamarie05@outlook.com>
2025-11-21 13:40:50 -0800

    Fix port

--- webarena/shopping/rebuild.sh
+++ webarena/shopping/rebuild.sh
@@ -6,8 +6,8 @@ docker container rm shopping
 docker build -t shopping:latest .
 
 if [[ "$arg" == "start" ]]; then
-    docker run --name shopping -p 9999:80 -p 8000:8000 -v $(pwd):/mnt/workspace -d shopping
-    docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://<your-server-hostname>:7770" # no trailing slash
-    docker exec shopping mysql -u magentouser -pMyPassword magentodb -e  'UPDATE core_config_data SET value="http://<your-server-hostname>:7770/" WHERE path = "web/secure/base_url";'
+    docker run --name shopping -p 7770:80 -p 8000:8000 -v $(pwd):/mnt/workspace -d shopping
+    docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://localhost:7770" # no trailing slash
+    docker exec shopping mysql -u magentouser -pMyPassword magentodb -e  'UPDATE core_config_data SET value="http://localhost:7770/" WHERE path = "web/secure/base_url";'
     docker exec shopping /var/www/magento2/bin/magento cache:flush
 fi
