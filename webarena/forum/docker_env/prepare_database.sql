c7da29fa0d53fa4950c783908c0abf99940d4f66
Adam Fourney <adamfo@microsoft.com>
2025-11-22 00:04:32 +0000

    Correct comment counts after deletions.

--- webarena/forum/docker_env/prepare_database.sql
+++ webarena/forum/docker_env/prepare_database.sql
@@ -1,6 +1,21 @@
 TRUNCATE submission_votes;
 DELETE FROM comments WHERE timestamp >= '2023-02-19 00:00:00+00';
 DELETE FROM submissions WHERE timestamp >= '2023-02-19 00:00:00+00';
+
+-- Update the comment counts in the submission table
+BEGIN;
+UPDATE submissions SET comment_count = 0;
+UPDATE submissions s
+  SET comment_count = c.cnt
+  FROM (
+    SELECT submission_id, COUNT(*) AS cnt
+    FROM comments
+    GROUP BY submission_id
+  ) AS c
+  WHERE c.submission_id = s.id;
+COMMIT;
+
+-- Clean up --
 SET max_parallel_maintenance_workers = 0;
 SET maintenance_work_mem = '64MB';
 VACUUM FULL ANALYZE;
