54ca235d0601de1f6b5eca65b137df68b773f4cd
Amanda Swearngin <mandamarie05@outlook.com>
2025-12-02 10:21:30 -0800

    Add pregenerated events.jsonl.gz file

--- webarena/shopping/docker_env/rebuild.sh
+++ webarena/shopping/docker_env/rebuild.sh
@@ -12,10 +12,4 @@ if [[ "$arg" == "start" ]]; then
     docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://localhost:7770" # no trailing slash
     docker exec shopping mysql -u magentouser -pMyPassword magentodb -e  'UPDATE core_config_data SET value="http://localhost:7770/" WHERE path = "web/secure/base_url";'
     docker exec shopping /var/www/magento2/bin/magento cache:flush
-
-    # Generate events inside the container once the container is fully started
-    # This is different than postmill because we need the web server to be running first to get an authenticatio token
-    # which we need to get existing product data for generating events from.
-    docker exec shopping python3 /var/www/html/generate_events.py --option init --num-products 10 --num-sales 10 --num-reviews 10 --num-restocks 10
-    docker exec shopping gzip /var/www/html/events.jsonl -c > /var/www/html/events.jsonl.gz
 fi
